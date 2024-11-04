ccb895b1f84e747706ec1e1b74cebfa1390f0780
0404233 <andrey0404233@gmail.com>
2024-11-04 18:35:49 +0300

    feat: implement CRUD API

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/product.model');
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/api/products', async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products)
+  } catch(err) {
+    res.status(500).json({message: err.message});
+  }
+})
+
+app.get('/api/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product)
+  } catch(err) {
+    res.status(500).json({message: err.message});
+  }
+})
+
+app.post('/api/products', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch(err) {
+    res.status(500).json({message: err.message})
+  }
+});
+
+
+app.put('/api/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      return res.status(404).json({message: "Product not found"});
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch(err) {
+    res.status(500).json({message: err.message});
+  }
+});
+
+app.delete('/api/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findByIdAndDelete(id);
+    
+    if(!product) {
+      return res.status(404).json({message: "Product not found"});
+    }
+    res.status(200).json({message: "Product deleted successfully"})
+  } catch(err) {
+    res.status(500).json({message: err.message});
+  }
+})
+
+mongoose.connect('mongodb+srv://admin:xxReyrWdctykjKnZ@backenddb.kcbhl.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB')
+  .then(() => {
+    console.log("Connected!");
+
+    app.listen(3000, () => {
+      console.log('Server is running')
+    });
+  })
+  .catch(() => {
+    console.log('Connection failed!')
+  })
